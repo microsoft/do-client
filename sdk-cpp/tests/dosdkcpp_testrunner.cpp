0ff81b1208d51f48d8db8b8c58e2cc7712f5ab9f
Jimmy Song <jimson@microsoft.com>
2021-04-13 14:07:45 -0700

    allow mcc override on all platforms

--- sdk-cpp/tests/dosdkcpp_testrunner.cpp
+++ sdk-cpp/tests/dosdkcpp_testrunner.cpp
@@ -16,21 +16,17 @@ int main(int argc, char** argv)
     po::options_description desc("Options");
     desc.add_options()
         ("help,h", "Show help messages")
-#if (DO_PLATFORM_ID == DO_PLATFORM_ID_LINUX)
         ("mcc-host,m", po::value<std::string>(), "MCC hostname to use (optional override)")
-#endif
-        ("manual-start", po::bool_switch()->default_value(false), "Wait for command line input to start tests (useful for debugging)");
+        ("manual-start,d", po::bool_switch()->default_value(false), "Wait for command line input to start tests (useful for debugging)");
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
 
-#if (DO_PLATFORM_ID == DO_PLATFORM_ID_LINUX)
     auto it = vm.find("mcc-host");
     if (it != vm.end())
     {
         g_mccHostName = it->second.as<std::string>();
         std::cout << "Got overriden MCC host: " << g_mccHostName << '\n';
     }
-#endif 
 
     auto manualStart = vm["manual-start"].as<bool>();
     if (manualStart)
