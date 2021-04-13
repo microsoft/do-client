68210e85e67561f9c6f7695cae6820eac1841ea5
Jimmy Song <jimson@microsoft.com>
2021-04-13 13:20:38 -0700

    refactor tests

--- sdk-cpp/tests/dosdkcpp_testrunner.cpp
+++ sdk-cpp/tests/dosdkcpp_testrunner.cpp
@@ -1,7 +1,12 @@
 #include "tests_common.h"
 
+#include <iostream>
+
 #include <boost/program_options.hpp>
+
+#if (DO_PLATFORM_ID == DO_PLATFORM_ID_LINUX)
 #include "test_data.h"
+#endif
 
 int main(int argc, char** argv)
 {
@@ -11,14 +16,29 @@ int main(int argc, char** argv)
     po::options_description desc("Options");
     desc.add_options()
         ("help,h", "Show help messages")
-        ("mcc-host,m", po::value<std::string>(), "MCC hostname to use (optional override)");
+#if (DO_PLATFORM_ID == DO_PLATFORM_ID_LINUX)
+        ("mcc-host,m", po::value<std::string>(), "MCC hostname to use (optional override)")
+#endif
+        ("manual-start", po::bool_switch()->default_value(false), "Wait for command line input to start tests (useful for debugging)");
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
+
+#if (DO_PLATFORM_ID == DO_PLATFORM_ID_LINUX)
     auto it = vm.find("mcc-host");
     if (it != vm.end())
     {
         g_mccHostName = it->second.as<std::string>();
         std::cout << "Got overriden MCC host: " << g_mccHostName << '\n';
+    }
+#endif 
+
+    auto manualStart = vm["manual-start"].as<bool>();
+    if (manualStart)
+    {
+        do
+        {
+            std::cout << '\n' << "Press a key to continue...";
+        } while (std::cin.get() != '\n');
     }
 
     return RUN_ALL_TESTS();
