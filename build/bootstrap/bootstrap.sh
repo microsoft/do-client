48fa282cd38bf2273c24d93f51f09bc4bef24fad
Jimmy Song <jimson@microsoft.com>
2021-06-29 15:49:30 -0700

    m

--- build/bootstrap/bootstrap.sh
+++ build/bootstrap/bootstrap.sh
@@ -3,13 +3,56 @@
 # bootstrap scripts will exit immediately if a command exits with a non-zero status
 set -e
 
-echo "Setting up development environment for do-client"
+# Defaults 
+COMMAND=all
 
-# Various development machine tools
-apt-get update
+usage() {
+    cat <<EOM
+$(basename $0) - Script to setup development environments for Delivery Optimization
+Usage: $(basename $0) --platform <platform> --command <command>
+	--platform			# Platform to provision, supported platforms: Ubuntu18.04, Ubuntu 20.04, Debian9, Debian 10. Default is Ubuntu18.04
+	--command 			# Which command to run, supported commands: build, developertools, containertools, qemu, all. Default is All
+EOM
+    exit 1
+}
 
-function installBuildDependencies()
+function parseArgs() {
+    arg_Positional=()
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+        --help | -h)
+            usage
+            shift
+            exit 0
+            ;;
+		--platform | -p)
+			PLATFORM="${2,,}"
+			if [[ "$PLATFORM" == "debian9" || "$PLATFORM" == "debian10" || "$PLATFORM" == "ubuntu18.04" || "$PLATFORM" == "ubuntu20.04" ]];
+			then
+				echo -e "[INFO] Platform set to: ${PLATFORM}"
+			else 
+				echo -e "[ERROR] Unsupported platform: ${PLATFORM}"
+				exit
+			fi
+			
+			shift
+			;;
+		--command | -c)
+			COMMAND="${2,,}"
+			echo -e "[INFO] Command to run set to: ${COMMAND}"
+			shift
+			;;
+        *)
+            arg_Positional+=("$1")
+            shift
+            ;;
+        esac
+    done
+}
+
+function installBuildDependencies
 {
+    echo "[INFO] Installing build dependencies"
     apt-get install -y build-essential g++ gdb gdbserver git wget
     apt-get install -y python3 cmake ninja-build rpm
 
@@ -45,29 +88,80 @@ function installBuildDependencies()
     make install
 }
 
-function installContainerTools()
+function installDeveloperTools
 {
-  apt install -y python-pip
-  pip install cpplint
-  # Installs to a non-standard location so add to PATH manually
-  export PATH=$PATH:~/.local/bin
+    echo "[INFO] Installing developer tools"
+    apt install -y python-pip
+    pip install cpplint
 
-  # Install docker to enable building cross-arch for arm
-  # Instructions located at: https://docs.docker.com/engine/install/ubuntu/
-  curl -fsSL https://get.docker.com -o get-docker.sh
-  sh get-docker.sh
-  # Install qemu for cross-arch support
-  apt-get -y install qemu binfmt-support qemu-user-static
+    # Installs to a non-standard location so add to PATH manually
+    export PATH=$PATH:~/.local/bin
+}
 
-  # Register qemu with docker to more easily run cross-arch containers
-  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes 
+function installContainerTools
+{
+    echo "[INFO] Installing Docker"
+    # Install docker to enable building cross-arch for arm
+    # Instructions located at: https://docs.docker.com/engine/install/ubuntu/
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+}
+
+function installQemu
+{
+    echo "[INFO] Installing Qemu for cross-arch support"
+    # Install qemu for cross-arch support
+    apt-get -y install qemu binfmt-support qemu-user-static
+
+    # Register qemu with docker to more easily run cross-arch containers
+    docker run --rm --privileged multiarch/qemu-user-static --reset -p yes 
+}
+
+function installAll
+{
+	echo "Setting up development environment for do-client"
+    installBuildDependencies
+    installDeveloperTools
+    installContainerTools
+    installQemu
 }
 
 main()
 {
-    installContainerTools
+    parseArgs "$@"
+	
+	if [[ -v PLATFORM ]]
+	then
+		echo "[INFO] Platform check susccesful"
+	else
+		echo "[ERROR] Please set a valid target platform"
+		exit
+	fi
+	
+	echo "[INFO] Updating package manager"
+    #apt-get update
+	
+	echo "[INFO] Running command: $COMMAND"
+	case $COMMAND in 
+	all)
+		installAll
+		;;
+	build)
+		installBuildDependencies
+		;;
+	developertools)
+		installDeveloperTools
+		;;
+	containertools)
+		installContainerTools
+		;;
+	qemu)
+		installQemu
+		;;
+	esac
+
+    echo "[INFO] Finished bootstrapping"
 }
 
-main
+main "$@"
 
-echo "Finished bootstrapping"
