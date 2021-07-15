cd2eaadc0755c93bb31234a19a4577aced9845e5
Jimmy Song <55897143+jimson-msft@users.noreply.github.com>
2021-07-15 15:42:00 -0700

    use ninja (#53)

--- build/docker/docker-build-plugin.sh
+++ build/docker/docker-build-plugin.sh
@@ -9,12 +9,12 @@ echo "Building apt plugin within Docker on Linux container"
 
 echo "Building & Installing sdk from source"
 cd $1
-python3 build/build.py --project sdk --cmaketarget deliveryoptimization --config $2 --generator "Unix Makefiles" --clean
+python3 build/build.py --project sdk --cmaketarget deliveryoptimization --config $2 --clean
 cd /tmp/build-deliveryoptimization-sdk/linux-$2/
 cmake --build . --target install
 
 cd $1
 echo "Building linux-apt plugin from source"
-python3 build/build.py --project plugin-apt --config $2 --package-for debian --generator "Unix Makefiles" --clean
+python3 build/build.py --project plugin-apt --config $2 --package-for debian --clean
 
 echo "Build of doclient-plugin completed"
