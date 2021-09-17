db4004ad519f1486ea113eda194e2fa3dd3a987c
Shishir Bhat <shishirb@microsoft.com>
2021-09-17 11:10:52 -0700

    Fix deb9 plugin build: cmake find_dependency macro     differs in functionality prior to 3.10 (#78)

--- sdk-cpp/build/cmake/deliveryoptimization_sdk-config.cmake
+++ sdk-cpp/build/cmake/deliveryoptimization_sdk-config.cmake
@@ -1,3 +1,8 @@
 include(CMakeFindDependencyMacro)
-find_dependency(Boost COMPONENTS filesystem system)
+if (${CMAKE_VERSION} VERSION_GREATER "3.9.0")
+    find_dependency(Boost COMPONENTS filesystem system)
+else ()
+    # Old cmake versions do not support extra args to find_dependency
+    find_dependency(Boost)
+endif ()
 include("${CMAKE_CURRENT_LIST_DIR}/deliveryoptimization_sdk-targets.cmake")
