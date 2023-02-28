9822cfd4023827fc34fc9a0e62ea6302c1aedbda
andretoyama-msft <andretoyama@microsoft.com>
2023-02-28 16:10:14 -0300

    add sdk-tests snap option

---
+++ build/build-snaps.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+set -e
+
+if [ $1 == "agent" -o $1 == "sdk" ]
+then
+    echo "Building $SNAP snap..."
+
+    if [ ! -d "/snap" ]
+    then
+        mkdir snap
+    fi
+
+    if [ $1 == "agent" ]
+    then
+        cp ./snapcraft-options/snapcraft-agent.yaml ./snap/snapcraft.yaml
+
+    else 
+        cp ./snapcraft-options/snapcraft-sdk.yaml ./snap/snapcraft.yaml
+    fi
+
+    snapcraft
+
+else
+    echo "$1 is not a valid snap option, valid options are 'agent' or 'sdk'"
+fi
