fa4d6d559ff817aa187fcd2f5238fb107c0f4903
Shishir Bhat <shishirb@microsoft.com>
2023-03-07 23:17:19 -0800

    Agent: Fix paths in snap build

--- client-lite/src/util/do_persistence.cpp
+++ client-lite/src/util/do_persistence.cpp
@@ -4,36 +4,60 @@
 #include "do_common.h"
 #include "do_persistence.h"
 
+#include <cstdlib>  // std::getenv
+
 namespace docli
 {
 
+static std::string ConstructPath(const char* suffix)
+{
+    std::string outputPath;
+#ifdef DO_BUILD_FOR_SNAP
+    const char* snapDataRoot = std::getenv("SNAP_DATA");
+    THROW_HR_IF(E_UNEXPECTED, (snapDataRoot == nullptr) || (*snapDataRoot == '\0'));
+    outputPath = snapDataRoot;
+    if (outputPath.back() != '/')
+    {
+        outputPath.push_back('/');
+    }
+    if (*suffix == '/')
+    {
+        ++suffix;
+    }
+    outputPath += suffix;
+#else
+    outputPath = suffix;
+#endif
+    return outputPath;
+}
+
 const std::string& GetLogDirectory()
 {
-    static std::string logDirectory(DO_AGENT_LOG_DIRECTORY_PATH);
+    static std::string logDirectory(ConstructPath(DO_AGENT_LOG_DIRECTORY_PATH));
     return logDirectory;
 }
 
 const std::string& GetRuntimeDirectory()
 {
-    static std::string runDirectory(DO_RUN_DIRECTORY_PATH);
+    static std::string runDirectory(ConstructPath(DO_RUN_DIRECTORY_PATH));
     return runDirectory;
 }
 
 const std::string& GetConfigDirectory()
 {
-    static std::string configDirectory(DO_CONFIG_DIRECTORY_PATH);
+    static std::string configDirectory(ConstructPath(DO_CONFIG_DIRECTORY_PATH));
     return configDirectory;
 }
 
 const std::string& GetSDKConfigFilePath()
 {
-    static std::string configFilePath(DO_CONFIG_DIRECTORY_PATH "/sdk-config.json");
+    static std::string configFilePath(ConstructPath(DO_CONFIG_DIRECTORY_PATH "/sdk-config.json"));
     return configFilePath;
 }
 
 const std::string& GetAdminConfigFilePath()
 {
-    static std::string configFilePath(DO_CONFIG_DIRECTORY_PATH "/admin-config.json");
+    static std::string configFilePath(ConstructPath(DO_CONFIG_DIRECTORY_PATH "/admin-config.json"));
     return configFilePath;
 }
 
