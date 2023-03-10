8b0bb60db658c21a3d45c0222c37f9454caca2c9
Shishir Bhat <shishirb@microsoft.com>
2023-03-09 21:47:41 -0800

    Remove platform arg from bootstrap.sh

--- build/scripts/bootstrap.sh
+++ build/scripts/bootstrap.sh
@@ -16,16 +16,22 @@ set -e
 
 # Defaults
 INSTALL=all
-PLATFORM=unknown
+
+# Distro and arch info
+OS=""
+VER=""
+is_linux=false
+is_macos=false
+is_amd64=false
+is_arm64=false
+is_arm32=false
 
 function usage {
     cat <<EOM
 $(basename $0) - Script to setup development environments for Delivery Optimization
-Usage: $(basename $0) --platform <platform to install for> --install <install command>
-    --platform          # Platform to provision, supported platforms: ubuntu1804, ubuntu2004, debian 10, osx. Required
+Usage: $(basename $0) --install <install command>
     --install           # Which command to run, supported commands: build, developertools, containertools, qemu, all. Default is all
 EOM
-    exit 1
 }
 
 function parseArgs {
@@ -36,18 +42,6 @@ function parseArgs {
             usage
             shift
             exit 0
-            ;;
-        --platform | -p)
-            PLATFORM="${2,,}"
-            if [[ "$PLATFORM" == "debian10" || "$PLATFORM" == "ubuntu1804" || "$PLATFORM" == "ubuntu2004" || "$PLATFORM" == "osx" ]];
-            then
-                echo -e "[INFO] Platform set to: ${PLATFORM}"
-            else
-                echo -e "[ERROR] Unsupported platform: ${PLATFORM}"
-                exit 1
-            fi
-
-            shift
             ;;
         --install | -i)
             INSTALL="${2,,}"
@@ -64,24 +58,16 @@ function parseArgs {
 
 function installBuildDependencies
 {
-    if [[ "$PLATFORM" != "unknown" ]]
-    then
-        echo "[INFO] Platform check succesful"
-    else
-        echo "[WARNING] No platform supplied, please supply a platform."
-        exit 1
-    fi
-
     echo "[INFO] Installing build dependencies"
 
-    if [[ "$PLATFORM" == "osx" ]];
+    if [[ $is_macos ]];
     then
         # Need to install homebrew to get all the above stuff
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         brew install cmake ninja g++ gdb gdbserver python3 git curl unzip tar pkg-config
 
         ./install-vcpkg-deps.sh ~/deliveryoptimization_tools/
-    elif isSupportedLinux
+    elif [[ $is_linux ]];
     then
 
         apt-get install -y make build-essential g++ gdb gdbserver gcc git wget
@@ -94,7 +80,7 @@ function installBuildDependencies
         mkdir /tmp/gtest
         pushd /tmp/gtest
 
-        if [[ "$PLATFORM" == "ubuntu2004" || "$PLATFORM" == "debian10" ]];
+        if [[ ($OS == "ubuntu" && $VER == "20.04") || ($OS == "debian" && $VER == "10") ]];
         then
             # The latest native-version of gtest on ubuntu2004 and debian10 currently has a bug where
             # CMakeLists doesn't declare an install target, causing 'make install' to fail.
@@ -125,10 +111,10 @@ function installBuildDependencies
 function installDeveloperTools
 {
     echo "[INFO] Installing developer tools"
-    if [[ "$PLATFORM" == "osx" ]]
+    if [[ $is_macos ]]
     then
         brew install cpplint
-    elif isSupportedLinux
+    elif [[ $is_linux ]]
     then
         apt-get install -y python-pip
         pip install cpplint
@@ -136,16 +122,14 @@ function installDeveloperTools
         # Installs to a non-standard location so add to PATH manually
         export PATH=$PATH:~/.local/bin
     else
-        "[INFO] Developer tools not supported on this platform"
+        echo "[INFO] Developer tools not supported on this platform"
     fi
 }
 
 function installContainerTools
 {
-    if !isSupportedLinux
+    if [[ $is_linux ]]
     then
-        echo "[INFO] Container builds not supported on this platform"
-    else
         apt-get install -y curl
 
         echo "[INFO] Installing Docker"
@@ -153,41 +137,39 @@ function installContainerTools
         # Instructions located at: https://docs.docker.com/engine/install/ubuntu/
         curl -fsSL https://get.docker.com -o get-docker.sh
         sh get-docker.sh
+    else
+        echo "[INFO] Container builds not supported on this platform"
     fi
 }
 
 function installQemu
 {
-    if !isSupportedLinux
+    if [[ $is_linux ]]
     then
-        echo "[INFO] Emulated builds not supported on this platform"
-    else
         echo "[INFO] Installing Qemu for cross-arch support"
         # Install qemu for cross-arch support
         apt-get -y install qemu binfmt-support qemu-user-static
 
         # Register qemu with docker to more easily run cross-arch containers
         docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+    else
+        echo "[INFO] Emulated builds not supported on this platform"
     fi
 }
 
 function installAll
 {
     echo "Setting up development environment for do-client"
-
     installBuildDependencies
-
-    if isSupportedLinux
-    then
-        installDeveloperTools
-        installContainerTools
-        installQemu
-    fi
+    installDeveloperTools
+    installContainerTools
+    installQemu
 }
 
 function isSupportedLinux()
 {
-    if [[ "$PLATFORM" == "debian10" || "$PLATFORM" == "ubuntu1804" || "$PLATFORM" == "ubuntu2004" ]];
+    if [[ ("$OS" == "ubuntu" && ($VER == "18.04" || $VER == "20.04"))
+        || ("$OS" == "debian" && ($VER == "10" || $VER == "11")) ]];
     then
         return 0
     else
@@ -195,12 +177,122 @@ function isSupportedLinux()
     fi
 }
 
+function isSupportedMacOS()
+{
+    return 1
+}
+
+# From DU project: https://github.com/Azure/iot-hub-device-update/blob/main/scripts/install-deps.sh
+function determine_machine_architecture()
+{
+    local arch=''
+    arch="$(uname -m)"
+    local ret_val=$?
+    if [[ $ret_val != 0 ]]; then
+        echo "Failed to get cpu architecture."
+        return 1
+    else
+        if [[ $arch == aarch64* || $arch == armv8* ]]; then
+            is_arm64=true
+        elif [[ $arch == armv7* || $arch == 'arm' ]]; then
+            is_arm32=true
+        elif [[ $arch == 'x86_64' || $arch == 'amd64' ]]; then
+            is_amd64=true
+        else
+            echo "Machine architecture '$arch' is not supported."
+            return 1
+        fi
+    fi
+}
+
+function determine_linux_distro()
+{
+    # Checking distro name and version
+    if [ -r /etc/os-release ]; then
+        # freedesktop.org and systemd
+        OS=$(grep "^ID\s*=\s*" /etc/os-release | sed -e "s/^ID\s*=\s*//")
+        VER=$(grep "^VERSION_ID=" /etc/os-release | sed -e "s/^VERSION_ID=//")
+        VER=$(sed -e 's/^"//' -e 's/"$//' <<< "$VER")
+    elif type lsb_release > /dev/null 2>&1; then
+        # linuxbase.org
+        OS=$(lsb_release -si)
+        VER=$(lsb_release -sr)
+    elif [ -f /etc/lsb-release ]; then
+        # For some versions of Debian/Ubuntu without lsb_release command
+        OS=$(grep DISTRIB_ID /etc/lsb-release | awk -F'=' '{ print $2 }')
+        VER=$(grep DISTRIB_RELEASE /etc/lsb-release | awk -F'=' '{ print $2 }')
+    elif [ -f /etc/debian_version ]; then
+        # Older Debian/Ubuntu/etc.
+        OS=Debian
+        VER=$(cat /etc/debian_version)
+    else
+        # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+        OS=$(uname -s)
+        VER=$(uname -r)
+    fi
+
+    # Convert OS to lowercase
+    OS="$(echo "$OS" | tr '[:upper:]' '[:lower:]')"
+}
+
+function determine_osx_ver()
+{
+    echo "Not implemented yet"
+    return 1
+}
+
+function determine_os_and_arch()
+{
+    if [[ $OSTYPE == linux* ]]; then
+        echo "Running on a linux-based OS"
+        is_linux=true
+        determine_linux_distro || return 1
+    elif [[ $OSTYPE == darwin* ]]; then
+        echo "Running on MacOS"
+        determine_osx_ver || return 1
+    else
+        echo "Unsupported OS '$OSTYPE'"
+        return 1
+    fi
+
+    determine_machine_architecture || return 1
+}
+
+function isSupportedOS()
+{
+    if [[ $is_linux ]]; then
+        if ! isSupportedLinux; then
+            echo "Unsupported Linux distro/version"
+            return 1
+        fi
+    elif [[ $is_macos ]]; then
+        if ! isSupportedMacOS; then
+            echo "Unsupported MacOS version"
+            return 1
+        fi
+    else
+        echo "Unsupported OS and/or Version"
+        return 1
+    fi
+    return 0
+}
+
 main()
 {
+    determine_os_and_arch || return 1
+
+    echo "OS = $OS"
+    echo "VER = $VER"
+    if [ $is_amd64 ]; then echo "Arch = amd64"
+    elif [ $is_arm32 ]; then echo "Arch = arm32"
+    elif [ $is_arm64 ]; then echo "Arch = arm64"
+    fi
+
+    isSupportedOS || return 1
+
     parseArgs "$@"
 
-    if isSupportedLinux
-    then
+    if [[ $is_linux ]]; then
         echo "[INFO] Updating package manager"
         apt-get update -y --fix-missing
     fi
