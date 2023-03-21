753b30bbc03d0ca3a6d650e93a860c50c8c266b6
Shishir Bhat <shishirb@microsoft.com>
2023-03-21 12:01:28 -0700

    Remove boost filesystem from bootstrap, snapcraft, and cmake config. NOTE: Ubuntu now complains about DO SDK itself including /include/deliveryoptimization ??sudo dpkg -i /tmp/build-deliveryoptimization-sdk/linux-debug/libdeliveryoptimization-dev_1.0.0_amd64.deb /tmp/build-deliveryoptimization-sdk/linux-debug/libdeliveryoptimization_1.0.0_amd64.deb

--- sdk-cpp/build/cmake/deliveryoptimization_sdk-config.cmake
+++ sdk-cpp/build/cmake/deliveryoptimization_sdk-config.cmake
@@ -1,11 +1,4 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-include(CMakeFindDependencyMacro)
-if (${CMAKE_VERSION} VERSION_GREATER "3.9.0")
-    find_dependency(Boost COMPONENTS filesystem system)
-else ()
-    # Old cmake versions do not support extra args to find_dependency
-    find_dependency(Boost)
-endif ()
 include("${CMAKE_CURRENT_LIST_DIR}/deliveryoptimization_sdk-targets.cmake")
