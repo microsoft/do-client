1c32132daf021409b7e743476b513925ce197455
Shishir Bhat <shishirb@microsoft.com>
2024-01-09 15:20:04 -0800

    Fix plugin yaml and shell script

--- build/docker/docker-build-plugin.sh
+++ build/docker/docker-build-plugin.sh
@@ -12,12 +12,20 @@ echo "Building apt plugin within Docker on Linux container"
 
 echo "Building & Installing sdk from source"
 cd $1
-python3 build/build.py --project sdk --cmaketarget deliveryoptimization --config $2 --clean
-cd /tmp/build-deliveryoptimization-sdk/linux-$2/
-cmake --build . --target install
+python3 build/build.py --project sdk --cmaketarget deliveryoptimization --config $2 --package-for DEB --clean
+pushd "/tmp/build-deliveryoptimization-sdk/linux-$2"
+dpkg --ignore-depends=deliveryoptimization-agent -i libdeliveryoptimization*.deb
+popd
 
-cd $1
 echo "Building linux-apt plugin from source"
 python3 build/build.py --project plugin-apt --config $2 --package-for debian --clean
 
 echo "Build of doclient-plugin completed"
+
+echo "Installing the plugin package"
+pushd "/tmp/build-deliveryoptimization-plugin-apt/linux-$2/"
+dpkg -i deliveryoptimization-plugin-apt*.deb
+popd
+
+echo "Removing packages"
+dpkg -r libdeliveryoptimization-dev deliveryoptimization-plugin-apt libdeliveryoptimization
