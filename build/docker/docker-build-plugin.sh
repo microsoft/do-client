6768d6babd31814bae0475513e9f9363ade4936d
Shishir Bhat <shishirb@microsoft.com>
2024-01-09 15:30:25 -0800

    Add comment blocks in plugin build script

--- build/docker/docker-build-plugin.sh
+++ build/docker/docker-build-plugin.sh
@@ -10,22 +10,27 @@ set -e
 
 echo "Building apt plugin within Docker on Linux container"
 
-echo "Building & Installing sdk from source"
+echo "***********************************************"
+echo "**** Building & Installing sdk from source ****"
+echo "***********************************************"
 cd $1
 python3 build/build.py --project sdk --cmaketarget deliveryoptimization --config $2 --package-for DEB --clean
 pushd "/tmp/build-deliveryoptimization-sdk/linux-$2"
+echo "**** Installing sdk package ****"
 dpkg --ignore-depends=deliveryoptimization-agent -i libdeliveryoptimization*.deb
 popd
 
-echo "Building linux-apt plugin from source"
+echo "***********************************************"
+echo "**** Building linux-apt plugin from source ****"
+echo "***********************************************"
 python3 build/build.py --project plugin-apt --config $2 --package-for debian --clean
 
-echo "Build of doclient-plugin completed"
-
-echo "Installing the plugin package"
+echo "***************************************"
+echo "**** Installing the plugin package ****"
+echo "***************************************"
 pushd "/tmp/build-deliveryoptimization-plugin-apt/linux-$2/"
 dpkg -i deliveryoptimization-plugin-apt*.deb
 popd
 
-echo "Removing packages"
+echo "**** Removing packages ****"
 dpkg -r libdeliveryoptimization-dev deliveryoptimization-plugin-apt libdeliveryoptimization
