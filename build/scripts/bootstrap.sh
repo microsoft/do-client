86997981e1a903ed09671c98010b3a877f037a81
Andre Ricardo <andre.ricardo@zoetis.com>
2025-03-17 19:12:23 +0000

    Add fix to build in Debian 12

--- build/scripts/bootstrap.sh
+++ build/scripts/bootstrap.sh
@@ -78,9 +78,9 @@ function installBuildDependencies
         mkdir /tmp/gtest
         pushd /tmp/gtest
 
-        if [[ ($DISTRO == "ubuntu" && ($VER == "20.04" || $VER == "22.04")) || ($DISTRO == "debian" && ($VER == "10" || $VER == "11")) ]];
+        if [[ ($DISTRO == "ubuntu" && ($VER == "20.04" || $VER == "22.04")) || ($DISTRO == "debian" && ($VER == "10" || $VER == "11" || $VER == "12")) ]];
         then
-            if [[ $VER == "22.04" ]]; then release="v1.13.0"; else release="release-1.10.0"; fi;
+            if [[ $VER == "22.04" || $VER == "12" ]]; then release="v1.13.0"; else release="release-1.10.0"; fi;
 
             # The latest native-version of gtest on the latest versions of ubuntu and debian currently has a bug where
             # CMakeLists doesn't declare an install target, causing 'make install' to fail.
